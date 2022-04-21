fb18f3f0e873094316d70341643ebe0ad6560544
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-20 18:41:53 -0700

    Completed challenge

--- main.js
+++ main.js
@@ -13,4 +13,29 @@ function changeColor () {
 }
 
 // Event Handler for Color Changer Function
-document.querySelector(('.color-changer')).addEventListener('click', changeColor);
+document.querySelector('.color-changer').addEventListener('click', changeColor);
+
+
+// Validate Text Input Function
+
+function validateText(evt) {
+    evt.preventDefault()
+
+    const formFeedback = document.querySelector('#formFeedback');
+
+    // Validate the input in the text input with id=number-input
+    const input = Number(document.querySelector('#number-input').value);
+    console.log('Input', input)
+    console.log('Type of', typeof 'input')
+    // If input is not a number or >= 10, change element with id=formFeedback
+    // to say 'Please enter a smaller number'.
+    // Otherwise, change text to 'You are good to go!'
+    if (isNaN(input) || input >=10 ){
+        formFeedback.innerHTML = 'Please enter a smaller number';
+    } else {
+        formFeedback.innerHTML = 'You are good to go!';
+    }
+}
+
+// Event Handler for Validate Text Input Function
+document.querySelector('input[type="submit"]').addEventListener('click', validateText);
